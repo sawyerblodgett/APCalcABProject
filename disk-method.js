adbf614d0eea79a61105850eabfc6f19b3c07faa
sawyerblodgett <sawyerblodgett@gmail.com>
2025-05-24 20:47:00 -0700

    Update disk-method.js

--- disk-method.js
+++ disk-method.js
@@ -34,7 +34,7 @@ function generateDisks(fxString, a, b, xSteps) {
       y: yArray,
       z: zArray,
       mode: "lines",
-      line: { color: "blue" },
+      line: { color: "green" },
       type: "scatter3d",
       hoverinfo: "none",
       showlegend: false,
